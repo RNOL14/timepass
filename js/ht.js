a9fd06899da453bccb3f0496a24fb1c6a0373ed6
RNOL14 <122205085+RNOL14@users.noreply.github.com>
2023-05-25 18:14:09 +0530

    Add files via upload

---
+++ js/ht.js
@@ -0,0 +1,11 @@
+
+    function myfunction() {
+
+        document.getElementById("khii").innerHTML="Ngyw khii janai manswi";
+        document.getElementById("khii").style.color="black";
+        document.getElementById("khii").style.fontSize="100px";
+
+
+        
+    }
+
